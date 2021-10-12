4c53eea3a499631beb50ec0c0e4861e71348a9d3
mohbayati <m.bayati14@gmail.com>
2021-10-12 13:05:08 +0330

    add middleware

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,9 +1,11 @@
 //import { createStore } from "redux";
 import { configureStore } from "@reduxjs/toolkit";
 import reducer from "./reducer";
+import logger from "./middleware/logger";
 
 export default function () {
   return configureStore({
     reducer,
+    middleware: [logger("console")],
   });
 }
