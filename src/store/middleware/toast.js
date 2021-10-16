e479f0ee18630786345b7d3eedb79354be1ad8c9
mohbayati <m.bayati14@gmail.com>
2021-10-16 10:05:30 +0330

    creating social test and axios mock

--- src/store/middleware/toast.js
+++ src/store/middleware/toast.js
@@ -1,7 +1,7 @@
 const toast = (store) => (next) => (action) => {
   if (action.type === "error") {
     console.log("Tostify ", action.payload.message);
-  } else next(action);
+  } else return next(action);
 };
 
 export default toast;
--- src/tests/bugs.spec.js
+++ src/tests/bugs.spec.js
@@ -1,5 +1,8 @@
+import Axios from "axios";
+import MockAdapter from "axios-mock-adapter";
 import { apiCallBegin } from "../store/api";
 import { addBug, addingBug } from "../store/bugs";
+import configureStore from "../store/configureStore";
 
 describe("bugsSlice", () => {
   describe("action creators", () => {
@@ -17,5 +20,19 @@ describe("bugsSlice", () => {
       };
       expect(result).toEqual(expected);
     });
+  });
+});
+
+describe("Social or behaiveral test", () => {
+  it("should handle the addBug action", async () => {
+    const bug = { describe: "a" };
+    const saveBug = { ...bug, id: 1 };
+
+    var mock = new MockAdapter(Axios);
+    mock.onPost("/bugs").reply(200, saveBug);
+    const store = configureStore();
+
+    await store.dispatch(addingBug(bug));
+    expect(store.getState().entites.bugs.list).toHaveLength(1);
   });
 });
